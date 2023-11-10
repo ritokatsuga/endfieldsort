049d9e35cb853f93925d769353f0bcf844017904
Ufal Salman <salman.ufal@gmail.com>
2023-11-10 21:36:48 +0700

    Started the project

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# endfieldsort
-Arknights: Endfield operator sorter
+# Arknights: Endfield Operator Sorter (endfieldsort)
+An Arknights: Endfield operator sorter with more features and filters that based on [execfera's Touhou Project character sorter](https://tohosort.frelia.my/) source code.
+
+Currently not published.
+
+Latest changelog:<br>
+10 November 2023 - Started the project
+
+Contributors are open
+
+Supports me via [Trakteer (Indonesia)](https://trakteer.id/ufalsalman/tip) or [PayPal (International)](https://paypal.me/ufalsalman)
+
+## Bahasa Indonesia
+
+Sebuah penyortir untuk operator Arknights: Endfield dengan fitur dan filter yang lebih banyak yang berdasarkan pada kode sumber [penyortir karakter Touhou Project milik execfera](https://tohosort.frelia.my/).
+
+Sekarang belum dipublikasikan. Versi bahasa Indonesia belum tersedia.
+
+Perubahan terakhir:<br>
+10 November 2023 - Memulai proyek
+
+Menerima kontributor.
+
+Dukung saya melalui [Trakteer (Indonesia)](https://trakteer.id/ufalsalman/tip) atau [PayPal (Internasional)](https://paypal.me/ufalsalman)
---
+++ src/js/data/2023-11-10.js
@@ -0,0 +1,114 @@
+dataSetVersion = "2023-11-10"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Filter by faction",
+    key: "faction",
+    tooltip: "Check this to restrict to operators from certain factions.",
+    checked: false,
+    sub: [
+      { name: "Endfield", key: "endfield" },
+      { name: "Landbreaker", key: "landbreaker" },
+      { name: "Order of Steel Oath", key: "oso" },
+      { name: "Talos-II General Chamber of Commerce", key: "tgcc" },
+      { name: "Rhodes Island", key: "rhodes" },
+      { name: "Hongshan", key: "hongshan" }
+    ]
+  },
+  {
+    name: "Exclude males",
+    key: "male",
+    tooltip: "Check this to exclude males.",
+    checked: false
+  },
+  {
+    name: "Exclude females",
+    key: "female",
+    tooltip: "Check this to exclude females.",
+    checked: false
+  }
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Endministrator (Female)",
+    img: "endminA.png",
+    opts: {
+      faction: ['endfield'],
+      female: true
+    }
+  },
+  {
+    name: "Endministrator (Male)",
+    img: "endminB.png",
+    opts: {
+      faction: ['endfield'],
+      male: true
+    }
+  },
+  {
+    name: "Perlica",
+    img: "perlica.png",
+    opts: {
+      faction: ['endfield'],
+      female: true
+    }
+  },
+  {
+    name: "Wulfgard",
+    img: "wulfgard.png",
+    opts: {
+      faction: ['landbreaker'],
+      male: true
+    }
+  },
+  {
+    name: "Xaihi",
+    img: "mybini.png",
+    opts: {
+      faction: ['ooso'],
+      female: true
+    }
+  },
+  {
+    name: "Ember",
+    img: "ember.png",
+    opts: {
+      faction: ['ooso'],
+      female: true
+    }
+  },
+  {
+    name: "Avywenna",
+    img: "avywenna.png",
+    opts: {
+      faction: ['tgcc'],
+      female: true
+    }
+  },
+  {
+    name: "Angelina",
+    img: "angelina.png",
+    opts: {
+      faction: ['rhodes'],
+      female: true
+    }
+  },
+  {
+    name: "Chen Qianyu",
+    img: "chen.png",
+    opts: {
+      faction: ['chen'],
+      female: true
+    }
+  },
+  {
+    name: "Fjall",
+    img: "fjall.png",
+    opts: {
+      faction: ['endfield'],
+      male: true
+    }
+  },
+]
